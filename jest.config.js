011f7b64b47f137d8de6d5eb52053b46722df45b
Silvio Moreira Galvão Junior <silviojuniorempresa@gmail.com>
2022-12-05 10:37:42 -0300

    chore: add configs

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  roots: '<rootDir>/src', 
+  roots: ['<rootDir>/src'], 
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
    coverageDirectory: 'coverage',
    testEnvironment: 'node',
    trasnform: {
-    '.+\\.ts$': 'ts-jest'
-   }
-};
+    '.+\\.ts': 'ts-jest'
+  }
+}
